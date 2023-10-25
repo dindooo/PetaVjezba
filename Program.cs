298a0565cb5cdc170ab3261623f262d19c950eca
dinooo <145918377+dindooo@users.noreply.github.com>
2023-10-25 09:30:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaPeta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite jedan cijeli i jedan decimalni broj; odvojite ih razmakom");
+            string niz = Console.ReadLine();
+            string[] a = niz.Split(' ');
+
+            int x = Convert.ToInt32(a[0]);
+            float y = Convert.ToSingle(a[1]);
+
+            Console.WriteLine(x * 2);
+            Console.WriteLine(y * 2);
+
+            Console.ReadKey();
+        }
+    }
+}
